12a2e129e5c771997e104c8b91ef6114cd2e5ad3
aryan karim <aryanmajid97@gmail.com>
2024-02-16 23:58:27 +0300

    chore(/shaders): add star fragment

--- src/entities/shaders/galaxy/fragment.glsl
+++ src/entities/shaders/galaxy/fragment.glsl
@@ -1,4 +1,9 @@
 void main()
 {
-    gl_FragColor = vec4(1.0);
+    vec2 vUv = gl_PointCoord;
+
+    float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
